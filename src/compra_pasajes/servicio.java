f7c8801251596e18d18560f02bc6ba38090dd36d
Alisson Muñoz <alisson.munoz@epn.edu.ec>
2024-06-14 12:17:36 -0500

    Primer commit

--- src/compra_pasajes/servicio.java
+++ src/compra_pasajes/servicio.java
@@ -10,7 +10,16 @@ public class servicio{
         this.precioBase = precioBase;
     }
 
+    public double getPrecioBase() {
+        return precioBase;
+    }
 
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+    public double calcularCosto(){
+        return precioBase ;
+    }
 }
 
 
